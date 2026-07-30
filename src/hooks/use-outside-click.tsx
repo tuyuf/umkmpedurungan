04088b42ab84fd6797e2a920d0a8bb0bb3547d78
tuyuf <fayyadhmuhammadhabibie@gmail.com>
2026-07-31 00:14:21 +0700

    fix(types): cast event.target as Node in useOutsideClick

--- src/hooks/use-outside-click.tsx
+++ src/hooks/use-outside-click.tsx
@@ -7,7 +7,7 @@ export const useOutsideClick = (
   useEffect(() => {
     const listener = (event: MouseEvent | TouchEvent) => {
       // DO NOTHING if the element being clicked is the target element or their children
-      if (!ref.current || ref.current.contains(event.target)) {
+      if (!ref.current || ref.current.contains(event.target as Node)) {
         return;
       }
       callback(event);
